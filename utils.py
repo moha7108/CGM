86554c413fabe45a63401eea40d0850bf69e6d76
moha7108 <moha7108@protonmail.com>
2023-04-01 17:31:49 -0400

    working autoencoder for face reconstruction

--- utils.py
+++ utils.py
@@ -8,3 +8,13 @@ def print_np_array_properties(np_array, name='np array'):
     print(f'=============== {name} Properties =============\n')
     print(f'{name}:\n {np_array}\n{name}_dtype: {np_array.dtype}\n{name}_shape: {np_array.shape}\n{name}_size: {np_array.size}\n')
     print('================================================')
+	
+	
+
+def split_validation(data):
+    
+    valid_len = len(data)//2
+    test_len = len(data) - valid_len
+    valid_data, test_data = torch.utils.data.random_split(data, [valid_len , test_len])
+    
+    return valid_data, test_data
